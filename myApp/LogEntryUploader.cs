46045be528e164c08eb23e914e8c7148f2847387
Pelle Frykman <pelle@pellefrykman.se>
2017-12-04 15:33:00 +0100

    Started working with DocumentDB client towards Cosmos DB.

--- myApp/LogEntryUploader.cs
+++ myApp/LogEntryUploader.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+using System;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Text;
@@ -17,11 +19,13 @@ namespace myApp
             client.PostAsync(url, content).Wait();
         }
 
-        //private static string EndpointUrl = "https://pellefrykman-cosmos1.documents.azure.com:443/";
-        //private static string PrimaryKey = "bdYhxMzTOl6uGrmm3yg4i02b5Fp6lqvvKw8Mpr8goi52YrSiMjcQb2ImDG0ffGZ3vC97cw4PouSUel6xq5liEg==";
+        private static string EndpointUrl = "https://pellefrykman-cosmos1.documents.azure.com:443/";
+        private static string PrimaryKey = "bdYhxMzTOl6uGrmm3yg4i02b5Fp6lqvvKw8Mpr8goi52YrSiMjcQb2ImDG0ffGZ3vC97cw4PouSUel6xq5liEg==";
 
         private static void UploadToCosmos(LogEntryInformation logEntryInfo) {
-            //DocumentClient client = new DocumentClient();
+            DocumentClient client = new DocumentClient(new Uri(LogEntryUploader.EndpointUrl), LogEntryUploader.PrimaryKey);
+            client.CreateDatabaseIfNotExistsAsync(new Database { id = "LogTracerEntry" });
+            
         }
     }
 }
